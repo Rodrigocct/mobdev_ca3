e4c62aff9681f249baae1a9cfa35f660908e7944
Rodrigocct <2022095@student.cct.ie>
2022-11-24 21:27:40 +0000

    Characters updated

--- mobdev_ca3/src/app/pages/characters-details/characters-details.page.ts
+++ mobdev_ca3/src/app/pages/characters-details/characters-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+
 
 @Component({
   selector: 'app-characters-details',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./characters-details.page.scss'],
 })
 export class CharactersDetailsPage implements OnInit {
+  character: any;
+  //isFavourite = false;
+  characterId = null;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit() {
+
+    this.characterId = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.api.getCharacter(this.characterId).subscribe(res => {this.character = res[0]; });
+
   }
 
 }
--- mobdev_ca3/src/app/pages/characters/characters.page.ts
+++ mobdev_ca3/src/app/pages/characters/characters.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+//import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-characters',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./characters.page.scss'],
 })
 export class CharactersPage implements OnInit {
+  characters: Observable<any>;
 
-  constructor() { }
+  constructor(private router: Router, private api: ApiService) { }
 
   ngOnInit() {
+    this.characters = this.api.getCharacters();
   }
+  openDetails(character){
+    // let split = character.url.split('/');
+      let characterId = character.char_id;
+      this.router.navigateByUrl(`/tab/characters/${characterId}`);
+    }
 
 }
