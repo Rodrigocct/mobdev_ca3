d6fd76183515ba42a7c864390a85648d1f046005
Rodrigocct <2022095@student.cct.ie>
2022-11-24 20:26:35 +0000

    Services updated

--- mobdev_ca3/src/app/app-routing.module.ts
+++ mobdev_ca3/src/app/app-routing.module.ts
@@ -2,43 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'tab',
-    loadChildren: () => import('./pages/tab/tab.module').then( m => m.TabPageModule)
-  },
-  {
-    path: 'characters',
-    loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
-  },
+ 
+ 
+ 
+  
   {
     path: 'characters-details',
     loadChildren: () => import('./pages/characters-details/characters-details.module').then( m => m.CharactersDetailsPageModule)
   },
-  {
-    path: 'episodes',
-    loadChildren: () => import('./pages/episodes/episodes.module').then( m => m.EpisodesPageModule)
-  },
+
   {
     path: 'episodes-details',
     loadChildren: () => import('./pages/episodes-details/episodes-details.module').then( m => m.EpisodesDetailsPageModule)
-  },
-  {
-    path: 'quotes',
-    loadChildren: () => import('./pages/quotes/quotes.module').then( m => m.QuotesPageModule)
-  },
-  {
-    path: 'death-count',
-    loadChildren: () => import('./pages/death-count/death-count.module').then( m => m.DeathCountPageModule)
-  },
+  }
 ];
 
 @NgModule({
